fae55f4af341cd326241c2598ff2455e0c2133c2
iferdel <ignaferdel@gmail.com>
2025-07-24 06:42:40 -0400

    get urls from html

---
+++ get_urls_from_html_test.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestGetURLsFromHTML(t *testing.T) {
+	tests := map[string]struct {
+		html     string
+		inputURL string
+		expected []string
+	}{
+		"single url": {
+			html: `
+			<html>
+				<body>
+					<a href="/path/one">
+						<span>Boot.dev</span>
+					</a>
+					<a href="https://other.com/path/one">
+						<span>Boot.dev</span>
+					</a>
+				</body>
+			</html>
+			`,
+			inputURL: "https://blog.boot.dev",
+			expected: []string{
+				"https://blog.boot.dev/path/one",
+				"https://other.com/path/one",
+			},
+		},
+	}
+
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			got, err := getURLsFromHTML(tc.html, tc.inputURL)
+			if err != nil {
+				t.Errorf("Test '%s' FAIL: unexpected error: %v", name, err)
+				return
+			}
+			if !reflect.DeepEqual(tc.expected, got) {
+				t.Errorf("expected %q, got %q", tc.expected, got)
+			}
+		})
+	}
+}
