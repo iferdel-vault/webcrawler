3765d05d44facbc630415fdf46cf304719a5765e
iferdel <ignaferdel@gmail.com>
2025-07-27 14:16:03 -0400

    recursive implemetation of traverse of html element nodes

--- get_urls_from_html.go
+++ get_urls_from_html.go
@@ -21,8 +21,30 @@ func getURLsFromHTML(htmlBody, rawBaseURL string) ([]string, error) {
 	}
 
 	var urls []string
-	urls, err = traverseNodesForURLS(doc, *baseURL)
+	err = traverseNodesForURLSRecursively(doc, *baseURL, &urls)
+	// urls, err = traverseNodesForURLS(doc, *baseURL)
 	return urls, nil
+}
+
+func traverseNodesForURLSRecursively(node *html.Node, baseURL url.URL, urls *[]string) error {
+
+	if node.Type == html.ElementNode && node.Data == "a" {
+		for _, anchor := range node.Attr {
+			href, err := url.Parse(anchor.Val)
+			if err != nil {
+				fmt.Printf("couldn't parse href '%v': %v\n", anchor.Val, err)
+				continue
+			}
+			resolvedURL := baseURL.ResolveReference(href)
+			*urls = append(*urls, resolvedURL.String())
+		}
+	}
+	// for each children(element) check if anchor and then run the recursion
+	for child := node.FirstChild; child != nil; child = child.NextSibling {
+		traverseNodesForURLSRecursively(child, baseURL, urls)
+	}
+
+	return nil
 }
 
 func traverseNodesForURLS(node *html.Node, baseURL url.URL) ([]string, error) {
